8d8a8b168eaec03f5a31094b806ece35c951aa5d
Lray227 <138041048+Lray227@users.noreply.github.com>
2023-09-27 16:50:44 -0300

    Site idade e sex

--- Exercicios/Aula012/Aula012Ex/Script2.js
+++ Exercicios/Aula012/Aula012Ex/Script2.js
@@ -1,16 +1,35 @@
-var data = new Date().getFullYear()
-var idade = document.getElementById('inum')
-var man = document.getElementById('Masc').value
-var woman = document.getElementById('Fem').value
+var ano = new Date().getFullYear()
+var data = document.getElementById('inum')
 var ans = document.getElementById('res')
+var sex = document.getElementsByName('sex')
 
-function cal(){
-    if (man = true){
-        var res = Number(data) - Number(idade.value)
-        var sex = 'um Homem'
-    } else if (woman = true){
-        var res = Number(data) - Number(idade.value)
-        var sex = 'uma Mulher'
+function Verificar(){
+    var idade = Number(ano) - Number(data.value)
+    var img = 
+    if (data.value.length == 0 || Number(data.value) > ano){
+        window.alert('[ERRO] Confira os dados')
+    }else if (sex[0].checked){
+        var gender = 'Homem'
+        if(idade >= 0 && idade < 10){
+            //bebe
+        }else if(idade < 21){
+            //jovem
+        }else if(idade < 50){
+            //adulto
+        }else {
+            //idoso
+        }
+    } else {
+        var gender = 'Mulher'
+        if(idade >= 0 && idade < 10){
+            //criança
+        }else if(idade < 21){
+            //jovem
+        }else if(idade < 50){
+            //adulto
+        }else {
+            //idoso
+        }
     }
-    ans.innerHTML = `Detectamos ${sex} de ${res} anos`
+    ans.innerHTML = `Detectamos ${gender} de ${idade} anos`
 }
