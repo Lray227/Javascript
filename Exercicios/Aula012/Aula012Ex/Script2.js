6b1c33e2413f2d56cdc9b08e24ea2a2476f49165
Lray227 <138041048+Lray227@users.noreply.github.com>
2023-09-27 10:09:06 -0300

    Update Script2.js

--- Exercicios/Aula012/Aula012Ex/Script2.js
+++ Exercicios/Aula012/Aula012Ex/Script2.js
@@ -1,14 +1,14 @@
 var data = new Date().getFullYear()
 var idade = document.getElementById('inum')
-var man = document.getElementById('Masc')
-var woman = document.getElementById('Fem')
+var man = document.getElementById('Masc').value
+var woman = document.getElementById('Fem').value
 var ans = document.getElementById('res')
 
 function cal(){
     if (man = true){
         var res = Number(data) - Number(idade.value)
         var sex = 'um Homem'
-    } else if (man = false){
+    } else if (woman = true){
         var res = Number(data) - Number(idade.value)
         var sex = 'uma Mulher'
     }
