80637543d80632e5f1745987a35b574e40919fe1
Lray227 <138041048+Lray227@users.noreply.github.com>
2023-10-02 18:17:13 -0300

    Site de conta

---
+++ Exercicios/Aula014/Aula014Ex/Script/Script.js
@@ -0,0 +1,13 @@
+var be = document.getElementById('iini').value
+var end = document.getElementById('ifim').value
+var r = document.getElementById('res')
+var button = document.getElementById('contar')
+var step = document.getElementById('istep').value
+
+button.addEventListener('click', Calc)
+
+function Calc(){
+    while (Number(be) <= Number(end)){
+        
+    }
+}
