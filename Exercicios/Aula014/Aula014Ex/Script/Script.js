c6b0fad1329f64f568fcb7fd4f35e7b1c53705fb
Lray227 <138041048+Lray227@users.noreply.github.com>
2023-10-03 03:42:03 -0300

    exercicio contador

--- Exercicios/Aula014/Aula014Ex/Script/Script.js
+++ Exercicios/Aula014/Aula014Ex/Script/Script.js
@@ -1,13 +1,38 @@
-var be = document.getElementById('iini').value
-var end = document.getElementById('ifim').value
-var r = document.getElementById('res')
-var button = document.getElementById('contar')
-var step = document.getElementById('istep').value
+var button = document.getElementById('conta')
 
-button.addEventListener('click', Calc)
+button.addEventListener('click', contar)
 
-function Calc(){
-    while (Number(be) <= Number(end)){
-        
+function contar(){
+    let ini = document.getElementById('txt1')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO] Faltam dados!')
+    }else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p <= 0){
+            window.alert('Passo inválido, considerando Passo 1')
+            p = 1
+        }
+
+        if (i < f){
+            for(let c = i; c <= f; c += p) {
+                //contagem crescente
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }else {
+            for(let c = i; c >= f; c -= p){
+                //contagem regressiva
+                res.innerHTML += `${c} \u{1F449}` 
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
