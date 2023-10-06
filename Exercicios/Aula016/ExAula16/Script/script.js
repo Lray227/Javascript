280493f8c1ead752de64bc3b3a717f4aaa5b6aa6
Lray227 <138041048+Lray227@users.noreply.github.com>
2023-10-06 14:13:37 -0300

    Inicio exercicio

---
+++ Exercicios/Aula016/ExAula16/Script/script.js
@@ -0,0 +1,9 @@
+let num = document.getElementById('num')
+let sel = document.getElementById('isel')
+
+function Adicionar(){
+    let n = [Number(num.value)]
+    if(num.value.length == 0){
+        window.alert('Insira os dados')
+    }
+}
