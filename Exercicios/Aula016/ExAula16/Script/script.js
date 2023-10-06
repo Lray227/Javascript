9581ee14f752fcfa30b10d6735dba2857260540f
Lray227 <138041048+Lray227@users.noreply.github.com>
2023-10-06 19:55:45 -0300

    Tentativa

--- Exercicios/Aula016/ExAula16/Script/script.js
+++ Exercicios/Aula016/ExAula16/Script/script.js
@@ -5,5 +5,12 @@ function Adicionar(){
     let n = [Number(num.value)]
     if(num.value.length == 0){
         window.alert('Insira os dados')
+    }else {
+        sel.innerText = ''
+        for(let pos in n){
+            let item = document.createElement('option')
+            item.text = `Adicionado o valor ${n}`
+            sel.appendChild(item)
+        }
     }
 }
