3a3243164792bfe09a434c8a8fec16a4c7b36231
Lray227 <138041048+Lray227@users.noreply.github.com>
2023-10-16 17:03:53 -0300

    Tentativa de progresso

--- Exercicios/Aula016/AulaEx016/Script/script.js
+++ Exercicios/Aula016/AulaEx016/Script/script.js
@@ -1,9 +1,15 @@
 let num = document.getElementById('num')
-let sel = document.getAnimations('sel')
+let sel = document.getElementById('sel')
 let valor = []
 
 function Adicionar(){
     if(Number(num.value) < 1 || Number(num.value) > 100){
         window.alert('[ERRO] Dados inválidos')
+    }else {
+        let n = Number(num.value)
+        let item = document.createElement('option')
+        item.text = `Adicionado o valor ${n}`
+        sel.appendChild(item)
+        valor.push(n)
     }
 }
